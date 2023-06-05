a3fe0ab4f9e333c0add12bf6a222c68d4abc4b95
elinanomade <elinanomad@gmail.com>
2023-06-05 23:39:33 +0200

    port

--- server/server.js
+++ server/server.js
@@ -1,1 +1,6 @@
-// THis holds backend
+// This holds backend
+const PORT = process.env.PORT ?? 8000;
+const express = require("express");
+const app = express();
+
+app.listen(PORT, () => console - console.log("Server running on PORT ${PORT}"));
