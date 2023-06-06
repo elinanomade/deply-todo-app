b4d183b0ae1870604651b382e440b13eb33e0849
elinanomade <elinanomad@gmail.com>
2023-06-06 18:16:57 +0200

    todos

--- server/server.js
+++ server/server.js
@@ -2,5 +2,21 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require("express");
 const app = express();
+const pool = require("./db");
+
+// This is rooting This code keep or not it was an example
+// app.get("/", (req, res) => {
+//   res.send("Hello Elina!");
+// });
+
+// get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await pool.query("SELECT * FROM todos");
+    res.json(todos.rows);
+  } catch (err) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT, () => console - console.log("Server running on PORT ${PORT}"));
